f95c33887c7a69e2875773db4ba1906445effde7
cyberwilly1 <68962141+cyberwilly1@users.noreply.github.com>
2020-12-02 10:05:38 -0400

    Update Pregunta7.java
    Solución propuesta Dos(O(n))

--- Pregunta7.java
+++ Pregunta7.java
@@ -18,6 +18,19 @@ public class Lolita {
         }
         return casa;
     }
+    
+    static int Solucion2(int barrio){
+        int casa = -1;
+        int sumabarrio = (barrio * (barrio + 1))/2;
+        for(int c = 2; c < barrio; c++){
+            int sumaiz = ((c - 1) * c)/2;
+            int sumade = sbarrio -sumaiz - c;
+            if(sumaiz == sumade){
+                casa = c;
+            }
+        }
+        return casa;
+    }
 
     public static void main(String[] args) {
         Solucion1(8);
