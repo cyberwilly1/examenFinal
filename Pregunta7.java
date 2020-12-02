bc3c96a5b3f926b7944dd26844f5bfae12d200f7
cyberwilly1 <68962141+cyberwilly1@users.noreply.github.com>
2020-12-02 10:04:28 -0400

    Update Pregunta7.java
    Solución propuesta Uno(O(n^2))

--- Pregunta7.java
+++ Pregunta7.java
@@ -1,3 +1,25 @@
 public class Lolita {
-    
+    static int Solucion1(int barrio){
+        int casa = -1;
+        for(int cas = 2; cas < barrio; cas++){
+            int sumaiz = 0;
+            for(int ci = 1; ci < cas; ci++){
+                sumaiz = sumaiz + ci;
+            }
+            
+            int sumade = 0;
+            for(int cd = cas + 1; cd <= barrio; cd++){
+                sumade = sumade + cd;
+            }
+
+            if(sumaiz == sumade){
+                casa = cas;
+            }
+        }
+        return casa;
+    }
+
+    public static void main(String[] args) {
+        Solucion1(8);
+    }
 }
